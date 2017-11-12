fcb803b74db2261740157b5b93674006652a4867
Lucas Silva <lucass.santos.silva@gmail.com>
2017-11-12 20:49:46 -0200

    Relacionando EquipamentosController com o Model

--- app/Http/Controllers/EquipamentosController.php
+++ app/Http/Controllers/EquipamentosController.php
@@ -3,10 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-// use App\Http\Request;
+use App\Http\Request;
+use App\Equipamento;
 
 class EquipamentosController extends Controller{
     public function index(){
+      $equipamento = Equipamento::all();
       return view('listaEquipamentos');
     }
 }
