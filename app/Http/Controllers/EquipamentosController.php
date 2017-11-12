96d085d139f2fcba55f6a99b9a88387615a8a090
Lucas Silva <lucass.santos.silva@gmail.com>
2017-11-12 19:07:48 -0200

    criando método index do EquipamentoController

--- app/Http/Controllers/EquipamentosController.php
+++ app/Http/Controllers/EquipamentosController.php
@@ -3,8 +3,10 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Request;
 
-class EquipamentosController extends Controller
-{
-    //
+class EquipamentosController extends Controller{
+    public function index(){
+      return view('listaEquipamentos')
+    }
 }
