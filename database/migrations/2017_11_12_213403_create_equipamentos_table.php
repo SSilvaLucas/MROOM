7495342164e2428b07315baf7d1cf32c42f70b30
Lucas Silva <lucass.santos.silva@gmail.com>
2017-11-12 19:41:10 -0200

    Criando Model e Migration para Equipamentos

---
+++ database/migrations/2017_11_12_213403_create_equipamentos_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEquipamentosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('equipamento', function (Blueprint $table) {
+            $table->engine = 'InnoDB';
+            $table->increments('idEquipamento');
+            // $table->integer('fkTipoEquipamento');
+            // $table->integer('fkAmbiente');
+            $table->integer('numeroEquipamento');
+            $table->date('ultimaManutencao');
+            $table->string('imagemEquipamento')->nullable($value = true);
+            $table->string('descricao', 200)->nullable($value = true);
+            $table->integer('manutencoesConcluídas');
+            $table->timestamps();
+
+            // $table->foreign('fkTipoEquipamento')
+            //       ->references('idTipoEquipamento')
+            //       ->on('tipo_equipamento');
+            // $table->foreign('fkAmbiente')
+            //       ->references('idAmbiente')
+            //       ->on('ambiente');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('equipamentos');
+    }
+}
